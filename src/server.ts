49cecd93cbc0ae05d2ee42f16f4485a0b4ff392b
Marcos Antonio <36957689+MarcosIf@users.noreply.github.com>
2022-03-28 21:34:04 -0300

    Criação do BD - METADE

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,8 @@
 import express, { request, response } from "express";
+import "reflect-metadata";
+import "./database";
 
 const app = express();
-app.get("/test", (request, response) => {
-   return response.send("Olá Mundo");
-});
 
-app.post("/test-post", (request, response) =>{
-
-    return response.send("Olá mundo - POST");
-});
 
 app.listen(3000, () => console.log("Servidor rodando"));
